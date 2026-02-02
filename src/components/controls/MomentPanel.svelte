21e5226474ea3d9a6f65e2200f68688c8fdaed7a
chivehao <chivehao@ikaros.run>
2026-02-03 02:59:46 +0800

    build: update MomentPanel.svelte

--- src/components/controls/MomentPanel.svelte
+++ src/components/controls/MomentPanel.svelte
@@ -23,8 +23,7 @@
     function formatDate(date: Date) {
         const month = (date.getMonth() + 1).toString().padStart(2, "0");
         const day = date.getDate().toString().padStart(2, "0");
-        const hour = date.getHours().toString().padStart(2, "0");
-        return `${month}-${day} ${hour}`;
+        return `${month}-${day}`;
     }
 
     function formatTag(tagList: string[]) {
