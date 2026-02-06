2a2373bb6a6e54c0282ae1bad6cc9946cc5c4195
chivehao <chivehao@ikaros.run>
2026-02-06 13:39:50 +0800

    Footer

---
+++ src/components/layout/BoYouQuanBadge.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+    import {siteConfig} from "@/content/config";
+
+    function getDomain(): string {
+        return siteConfig.site_url.replace("http://", "").replace("https://", "")
+    }
+
+    const isDarkMode =
+        document.documentElement.classList.contains("dark") ||
+        window.matchMedia("(prefers-color-scheme: dark)").matches;
+</script>
+<div style="display: inline;">
+    <a href={'https://www.boyouquan.com/certificates/' + getDomain()} target="_blank" rel="nofollow"
+       class="badge boyouquan">
+        <img src={ isDarkMode
+        ? 'https://www.boyouquan.com/images/logo/performance-dark.svg?domainName='
+        : 'https://www.boyouquan.com/images/logo/performance.svg?domainName='
+        + getDomain()}
+             alt="博友圈"
+             style="display:block;height:28px;">
+    </a>
+</div>
--- src/content/config/footerConfig.ts
+++ src/content/config/footerConfig.ts
@@ -3,6 +3,7 @@ import type { FooterConfig } from "../../types/config.ts";
 export const footerConfig: FooterConfig = {
 	// 是否启用Footer HTML注入功能
 	enable: true,
+	icp: "赣ICP备2025068655号-1",
 };
 
 // 直接编辑 config/FooterConfig.html 文件来添加备案号等自定义内容
