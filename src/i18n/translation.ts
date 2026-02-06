0c72d79147259be4b60f58d55668f6c2179f456e
chivehao <chivehao@ikaros.run>
2026-02-06 21:03:29 +0800

    修复编译问题

--- src/i18n/translation.ts
+++ src/i18n/translation.ts
@@ -2,7 +2,6 @@ import { siteConfig } from "../content/config";
 import type I18nKey from "./i18nKey";
 import { en } from "./languages/en";
 import { ja } from "./languages/ja";
-import { ru } from "./languages/ru";
 import { zh_CN } from "./languages/zh_CN";
 import { zh_TW } from "./languages/zh_TW";
 
@@ -21,8 +20,6 @@ const map: { [key: string]: Translation } = {
 	zh_tw: zh_TW,
 	ja: ja,
 	ja_jp: ja,
-	ru: ru,
-	ru_ru: ru,
 };
 
 export function getTranslation(lang: string): Translation {
