2909534bbfd6de43b2e46c8af0c427e902c355bd
chivehao <chivehao@ikaros.run>
2026-02-06 20:57:53 +0800

    首页不显示日记

--- src/content/config/diary.config.ts
+++ src/content/config/diary.config.ts
@@ -3,5 +3,5 @@ import type {DiaryConfig} from "@/types/diary.ts";
 export const diaryConfig: DiaryConfig = {
     enable: true,
     utcTimeZone: 8,
-    displayInHome: true,
+    displayInHome: false,
 }
