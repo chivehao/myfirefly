a6897228c388ed97184179d8263fc8f07c29536b
chivehao <chivehao@ikaros.run>
2026-02-07 22:23:32 +0800

    将日记内容加进rss的item里

--- src/pages/rss.xml.ts
+++ src/pages/rss.xml.ts
@@ -2,8 +2,8 @@ import { loadRenderers } from "astro:container";
 import { render } from "astro:content";
 import { getContainerRenderer as getMDXRenderer } from "@astrojs/mdx";
 import rss, { type RSSFeedItem } from "@astrojs/rss";
-import { getSortedPosts } from "@utils/content-utils";
-import { formatDateI18nWithTime } from "@utils/date-utils";
+import {getDiaries, getSortedPosts} from "@utils/content-utils";
+import {formatDateI18nWithTime, parseDateFromPath, parseFileNameFromPath} from "@utils/date-utils";
 import { url } from "@utils/url-utils";
 import type { APIContext } from "astro";
 import { experimental_AstroContainer as AstroContainer } from "astro/container";
@@ -20,10 +20,26 @@ function stripInvalidXmlChars(str: string): string {
 }
 
 export async function GET(context: APIContext) {
+	const diaries = await getDiaries();
 	const blog = await getSortedPosts();
 	const renderers = await loadRenderers([getMDXRenderer()]);
 	const container = await AstroContainer.create({ renderers });
 	const feedItems: RSSFeedItem[] = [];
+	for (const diary of diaries) {
+		const { Content } = await render(diary);
+		const rawContent = await container.renderToString(Content);
+		const cleanedContent = stripInvalidXmlChars(rawContent);
+		const fileName = parseFileNameFromPath(diary.filePath as string);
+		feedItems.push({
+			title: "日记: " + fileName,
+			pubDate: parseDateFromPath(diary.filePath as string),
+			description: "分享每一天",
+			link: url(`/diaries/${fileName}/`),
+			content: sanitizeHtml(cleanedContent, {
+				allowedTags: sanitizeHtml.defaults.allowedTags.concat(["img"]),
+			}),
+		})
+	}
 	for (const post of blog) {
 		const { Content } = await render(post);
 		const rawContent = await container.renderToString(Content);
@@ -38,6 +54,11 @@ export async function GET(context: APIContext) {
 			}),
 		});
 	}
+	// 按照 pubDate 从最近到最远排序
+	feedItems.sort((f1, f2) => {
+		// @ts-ignore
+		return f1.pubDate > f2.pubDate ? -1 : 1;
+	})
 	return rss({
 		title: siteConfig.title,
 		description: siteConfig.subtitle || "No description",
