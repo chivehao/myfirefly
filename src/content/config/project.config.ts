e6b44d799c4754a673c92c4d2e59755adf155598
chivehao <chivehao@ikaros.run>
2026-02-09 05:57:46 +0800

    更新 project.config.ts

--- src/content/config/project.config.ts
+++ src/content/config/project.config.ts
@@ -33,6 +33,16 @@ export const projects: ProjectItem[] = [
         status: "finished",
     },
     {
+        id: "endfield-novel",
+        type: "novel",
+        title: "终末地：开拓，失忆和管理员",
+        description: "明日方舟终末地的同人小说。",
+        url: "https://fanqienovel.com/page/7603967738356190233",
+        cover: "https://imgbed.ikaros.run/file/posts/1770587532432_20260209055207511.png",
+        tags: ["长篇", "科幻", "末世", "衍生", "同人", "架空"],
+        status: "active",
+    },
+    {
         id: "myfirst-novel",
         type: "novel",
         title: "大奉帝国",
