fdffd81a17c1785eb725d5a5ca4e4c5ed27f6b34
chivehao <chivehao@ikaros.run>
2026-02-10 21:49:41 +0800

    Revert "将日记配置从ts文件转移src/content/config.yaml文件里"
    This reverts commit c91c42a0ef3fe539137a0085a780e32cf3793b1b.

---
+++ src/content/config/diary.config.ts
@@ -0,0 +1,10 @@
+import type {DiaryConfig} from "@/types/diary.ts";
+
+export const diaryConfig: DiaryConfig = {
+    enable: false,
+    utcTimeZone: 8,
+    displayInHome: true,
+    comment: true,
+    add2Rss: false,
+    displayInStatic: true,
+}
--- src/types/diary.ts
+++ src/types/diary.ts
@@ -1,21 +1,13 @@
 // @ts-ignore
 import type {AstroComponentFactory} from "astro/runtime/server";
-import {z} from "astro/zod";
-import { loadRawConfig } from "../utils/config-utils"
 
-export const DiaryConfigSchema = z.object({
-    enable: z.boolean(),
-    displayInHome: z.boolean().optional().default(false), // 是否在首页显示
-    utcTimeZone: z.number().int().min(-12).max(14).optional().default(8), // 日记列表页计算时间用的时区
-    comment: z.boolean().optional().default(false), // 是否开启评论
-    add2Rss: z.boolean().optional().default(false), // 是否添加到rss里
-    displayInStatic: z.boolean().optional().default(false), // 是否在站点统计展示
-})
-export type DiaryConfig = z.infer<typeof DiaryConfigSchema>;
-export function loadDiaryConfig(): DiaryConfig {
-    // @ts-ignore
-    const rawDiaryConfig = loadRawConfig()?.diaryConfig ?? {};
-    return DiaryConfigSchema.parse(rawDiaryConfig);
+export type DiaryConfig = {
+    enable: boolean;
+    displayInHome?: boolean; // 是否在首页显示
+    utcTimeZone: number;
+    comment?: boolean; // 是否开启评论
+    add2Rss?: boolean; // 是否添加到rss里
+    displayInStatic?: boolean; // 是否在站点统计展示
 }
 
 export type DiaryItem = {
