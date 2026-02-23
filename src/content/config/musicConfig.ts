60640628d5db1ee94add81639f4cce7f867d7fd2
chivehao <chivehao@ikaros.run>
2026-02-23 20:23:58 +0800

    musicConfig.ts

--- src/content/config/musicConfig.ts
+++ src/content/config/musicConfig.ts
@@ -6,7 +6,7 @@ export const musicPlayerConfig: MusicPlayerConfig = {
 	enable: true,
 
 	// 使用方式："meting" 使用 Meting API，"local" 使用本地音乐列表
-	mode: "local",
+	mode: "meting",
 
 	// Meting API 配置
 	meting: {
